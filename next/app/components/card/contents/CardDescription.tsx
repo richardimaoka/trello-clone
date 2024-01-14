805445e75676734f92fa4a357d1a1eb55d659604
Richard Imaoka <richard.e.imaoka@gmail.com>
2024-01-14 16:12:04 +0900

    contents

--- next/app/components/card/contents/CardDescription.tsx
+++ next/app/components/card/contents/CardDescription.tsx
@@ -1,11 +1,23 @@
+import styles from "./CardDescription.module.css";
+import { EditButton } from "./EditButton";
+
 interface Props {}
 
 export function CardDescription(props: Props) {
   return (
-    <div>
-      <div>Description</div>
-      <div>Edit</div>
-      <div>area</div>
+    <div className={styles.component}>
+      <div className={styles.side}>-</div>
+
+      <div className={styles.body}>
+        <div className={styles.header}>
+          <div>Description</div>
+          <div>
+            <EditButton />
+          </div>
+        </div>
+
+        <p>https://l-tike.com/event/mevent/?mid=706609</p>
+      </div>
     </div>
   );
 }
---
+++ next/app/components/card/contents/EditButton.tsx
@@ -0,0 +1,7 @@
+import styles from "./EditButton.module.css";
+
+interface Props {}
+
+export function EditButton(props: Props) {
+  return <button className={styles.component}>Edit</button>;
+}
